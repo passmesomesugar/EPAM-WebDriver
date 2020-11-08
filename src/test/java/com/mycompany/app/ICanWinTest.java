f4196f33935b88c0728c9153093cc9cb5d5ba61f
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2020-11-08 11:57:29 +0600

    test working, afterMethod  added

--- src/test/java/com/mycompany/app/ICanWinTest.java
+++ src/test/java/com/mycompany/app/ICanWinTest.java
@@ -19,8 +19,6 @@ public class ICanWinTest {
 
     @Test
     public void createPasteBtnTest() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\PC\\Desktop\\Selenium\\chromedriver_win32\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
         driver.get("https://pastebin.com/");
         driver.manage().window().maximize();
 //
